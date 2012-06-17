f373e1fb7c06427c722a2d4a6fc044779449e156
Joe Alcorn <me@buttscicl.es>
2012-06-17 11:53:42 +0100

    Improve the 'reqs per day' graph

--- graphs.py
+++ graphs.py
@@ -11,7 +11,6 @@ def active_mods(data):
     return d
 
 def modreqs_per_day(data, limit):
-    tempTimes = []
     times = {}
     points = []
 
@@ -20,15 +19,13 @@ def modreqs_per_day(data, limit):
         today = datetime.date.today()
         margin = datetime.timedelta(days = limit)
         if today - margin <= reqTime <= today + margin:
-            tempTimes.append(reqTime)
+            reqTime = datetime.date.today() - reqTime
+            reqTime = re.sub("[^0-9]", "", str(reqTime)[:2])
+            if not times.has_key(reqTime):
+                times[reqTime] = 1
+            else:
+                times[reqTime] += 1
 
-    for time in tempTimes:
-        time = datetime.date.today() - time
-        time = re.sub("[^0-9]", "", str(time)[:2])
-        if not times.has_key(time):
-            times[time] = 1
-        else:
-            times[time] += 1
     for key in times:
         points.append(times[key])
 
